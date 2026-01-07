3e6089d3d77dee892033971db23bdc892ae628d9
m1sh4k <mishaivan373@gmail.com>
2026-01-07 16:36:44 +0300

    add: root dir for calculating links

--- src/tree_toc_md/main.py
+++ src/tree_toc_md/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import sys
 
@@ -6,39 +7,60 @@ from tree_toc_md.build_tok import build_toc
 
 
 def main():
-    if len(sys.argv) < 2 or '-h' in sys.argv:
-        print(
-            "Usage: python toc_gen.py <directory> [-e] [-o|-g]",
-            file=sys.stderr
+    parser = argparse.ArgumentParser(
+        description="Generate a Markdown Table of Contents tree.",
+        usage="toc-md -d <dir> [-r <root>] [-e] [-o|-g]"
+    )
+
+    parser.add_argument(
+        "-d", "--dir",
+        default=".",
+        help="Directory to scan (default: current directory)"
+    )
+    parser.add_argument(
+        "-r", "--root",
+        default=os.getcwd(),
+        help=(
+            "Root directory for relative links"
+            "(default: current working directory)"
         )
-        print("", file=sys.stderr)
-        print("Options:", file=sys.stderr)
-        print("  -e          Include H1 headings from files", file=sys.stderr)
-        print("  -h          Help (this message)", file=sys.stderr)
-        print("  -o          Obsidian format (Wikilinks)", file=sys.stderr)
-        print("  -g          GitHub/Gitea format (default)", file=sys.stderr)
+    )
+    parser.add_argument(
+        "-e", "--extract-h1",
+        action="store_true",
+        help="Include H1 headings from files"
+    )
+
+    format_group = parser.add_mutually_exclusive_group()
+    format_group.add_argument(
+        "-o", "--obsidian",
+        action="store_true",
+        help="Obsidian format (Wikilinks)"
+    )
+    format_group.add_argument(
+        "-g", "--github",
+        action="store_true",
+        help="GitHub/Gitea format (default)"
+    )
+
+    args = parser.parse_args()
+
+    format_type = 'obsidian' if args.obsidian else 'github'
+
+    if not os.path.isdir(args.dir):
+        print(f"Error: Directory '{args.dir}' does not exist", file=sys.stderr)
         sys.exit(1)
 
-    directory = sys.argv[1]
-    use_h1 = '-e' in sys.argv
+    abs_scan_dir = os.path.abspath(args.dir)
+    abs_link_root = os.path.abspath(args.root)
 
-    if '-g' in sys.argv:
-        format_type = 'github'
-    elif '-o' in sys.argv:
-        format_type = 'obsidian'
-    else:
-        format_type = 'github'
-
-    if not os.path.isdir(directory):
-        print(
-            f"Error: Directory '{directory}' does not exist",
-            file=sys.stderr)
-        sys.exit(1)
-
-    abs_dir = os.path.abspath(directory)
-    print(build_toc(abs_dir, use_h1, format_type, root_path=abs_dir))
+    print(build_toc(
+        root_dir=abs_scan_dir,
+        use_h1=args.extract_h1,
+        format_type=format_type,
+        root_path=abs_link_root
+    ))
 
 
 if __name__ == "__main__":
     main()
-
